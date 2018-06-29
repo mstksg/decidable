200f9bd2208a0684701370114e9d0f4f0d35c98b
Justin Le <justin@jle.im>
2018-06-29 16:23:55 -0700

    split out subset module

--- src/Data/Type/Quantification.hs
+++ src/Data/Type/Quantification.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE AllowAmbiguousTypes   #-}
+{-# LANGUAGE EmptyCase             #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE GADTs                 #-}
@@ -18,9 +19,6 @@ module Data.Type.Quantification (
   , entailAny, ientailAny, entailAnyF, ientailAnyF
   , All, WitAll(..)
   , entailAll, ientailAll, entailAllF, ientailAllF, decideEntailAll, idecideEntailAll
-  , Subset, WitSubset(..)
-  , makeSubset, subsetToList, subsetToAny, subsetToAll
-  , intersection, union, symDiff, mergeSubset, imergeSubset
   ) where
 
 import           Control.Applicative
@@ -30,85 +28,6 @@ import           Data.Singletons
 import           Data.Singletons.Decide
 import           Data.Type.Predicate
 import           Data.Type.Universe
-
--- | A @'Subset' p as@ describes a /decidable/ subset of type-level
--- collection @as@.
-newtype WitSubset f p (as :: f k) = WitSubset
-    { runWitSubset :: forall a. Elem f as a -> Decision (p @@ a)
-    }
-
-data Subset f :: (k ~> Type) -> (f k ~> Type)
-type instance Apply (Subset f p) as = WitSubset f p as
-
-instance (Universe f, Decide p) => Decide (Subset f p)
-instance (Universe f, Decide p) => Taken (Subset f p) where
-    taken = makeSubset @f @_ @p (\_ -> decide @p)
-
--- | Create a 'Subset' from a predicate.
-makeSubset
-    :: forall f k p (as :: f k). Universe f
-    => (forall a. Elem f as a -> Sing a -> Decision (p @@ a))
-    -> Sing as
-    -> Subset f p @@ as
-makeSubset f xs = WitSubset $ \i -> f i (select i xs)
-
--- | Turn a 'Subset' into a list (or any 'Alternative') of satisfied
--- predicates.
-subsetToList
-    :: forall f p t. (Universe f, Alternative t)
-    => (Subset f p --># Any f p) t
-subsetToList xs s = getAlt $ (`ifoldMapUni` xs) $ \i _ -> Alt $ case runWitSubset s i of
-    Proved p    -> pure $ WitAny i p
-    Disproved _ -> empty
-
--- | Restrict a 'Subset' to a single (arbitrary) member, or fail if none
--- exists.
-subsetToAny
-    :: forall f p. Universe f
-    => Subset f p -?> Any f p
-subsetToAny xs s = idecideAny (\i _ -> runWitSubset s i) xs
-
--- | Combine two subsets based on a decision function
-imergeSubset
-    :: forall f k p q r (as :: f k). ()
-    => (forall a. Elem f as a -> Decision (p @@ a) -> Decision (q @@ a) -> Decision (r @@ a))
-    -> Subset f p @@ as
-    -> Subset f q @@ as
-    -> Subset f r @@ as
-imergeSubset f ps qs = WitSubset $ \i ->
-    f i (runWitSubset ps i) (runWitSubset qs i)
-
--- | Combine two subsets based on a decision function
-mergeSubset
-    :: forall f k p q r (as :: f k). ()
-    => (forall a. Decision (p @@ a) -> Decision (q @@ a) -> Decision (r @@ a))
-    -> Subset f p @@ as
-    -> Subset f q @@ as
-    -> Subset f r @@ as
-mergeSubset f = imergeSubset (\(_ :: Elem f as a) p -> f @a p)
-
--- | Subset intersection
-intersection
-    :: forall f p q. ()
-    => ((Subset f p &&& Subset f q) --> Subset f (p &&& q))
-intersection _ = uncurry $ imergeSubset $ \(_ :: Elem f as a) -> proveAnd @p @q @a
-
--- | Subset union
-union
-    :: forall f p q. ()
-    => ((Subset f p &&& Subset f q) --> Subset f (p ||| q))
-union _ = uncurry $ imergeSubset $ \(_ :: Elem f as a) -> proveOr @p @q @a
-
-symDiff
-    :: forall f p q. ()
-    => ((Subset f p &&& Subset f q) --> Subset f (p ^^^ q))
-symDiff _ = uncurry $ imergeSubset $ \(_ :: Elem f as a) -> proveXor @p @q @a
-
--- | Test if a subset is equal to the entire original collection
-subsetToAll
-    :: forall f p. Universe f
-    => Subset f p -?> All f p
-subsetToAll xs s = idecideAll (\i _ -> runWitSubset s i) xs
 
 -- | If there exists an @a@ s.t. @p a@, and if @p@ implies @q@, then there
 -- must exist an @a@ s.t. @q a@.
